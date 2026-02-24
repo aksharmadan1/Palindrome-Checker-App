87beb77a55f3c21b283a3af6aef9d6be24c95f02
aksharmadan1 <aksharmadan@gmail.com>
2026-02-24 14:55:58 +0530

    UC11: Refactored logic into PalindromeService class

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
 public class Main {
-    public static boolean isPalindrome(String word) {
-        // Regex: \\s+ matches one or more spaces
-        String clean = word.replaceAll("\\s+", "").toLowerCase();
-
-        int left = 0;
-        int right = clean.length() - 1;
-
-        while (left < right) {
-            if (clean.charAt(left) != clean.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        String input = "Nurses Run";
-        System.out.println("Checking phrase: " + input);
-        System.out.println(isPalindrome(input) ? "Result: Palindrome" : "Result: Not Palindrome");
+        PalindromeService service = new PalindromeService();
+        String test = "Was it a car or a cat I saw";
+
+        System.out.println("Testing OOP Service:");
+        System.out.println("Input: " + test);
+        System.out.println("Result: " + (service.isPalindrome(test) ? "Palindrome" : "Not Palindrome"));
     }
 }
