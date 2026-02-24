a556f44b6b51c6235df0b10a8995951d02eed30f
aksharmadan1 <aksharmadan@gmail.com>
2026-02-24 16:07:03 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+public class UseCase1PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+        // Application Startup Behavior
+        System.out.println("========================================");
+        System.out.println("   WELCOME TO PALINDROME CHECKER APP   ");
+        System.out.println("========================================");
+        System.out.println("Application Name: Palindrome Checker");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Status: System Ready...");
+        System.out.println("----------------------------------------");
     }
 }
