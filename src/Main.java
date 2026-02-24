b69f8b857dfd777e146ed9bd5f665c6081446eaf
aksharmadan1 <aksharmadan@gmail.com>
2026-02-24 14:50:56 +0530

    UC3: Added User Input functionality

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
-    public static boolean isPalindrome(String word) {
-        // Cleaning the string: remove spaces and lowercase
-        String clean = word.replaceAll("\\s+", "").toLowerCase();
-        int left = 0, right = clean.length() - 1;
-        while (left < right) {
-            if (clean.charAt(left) != clean.charAt(right)) return false;
-            left++; right--;
-        }
-        return true;
-    }
     public static void main(String[] args) {
-        String input = "Madam In Eden Im Adam";
-        System.out.println(isPalindrome(input) ? "Palindrome" : "Not Palindrome");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Palindrome Checker App");
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println("Result: Palindrome");
+        } else {
+            System.out.println("Result: Not Palindrome");
+        }
+        scanner.close();
     }
 }
