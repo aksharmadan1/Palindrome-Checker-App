2f1979790afb116ac12fb1001ce3c3ad4833d7e7
aksharmadan1 <aksharmadan@gmail.com>
2026-02-24 14:50:09 +0530

    UC2: Logic update for main file

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
-public class Main public class Main {
-
+public class Main {
+    public static boolean isPalindrome(String word) {
+        // Cleaning the string: remove spaces and lowercase
+        String clean = word.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = clean.length() - 1;
+        while (left < right) {
+            if (clean.charAt(left) != clean.charAt(right)) return false;
+            left++; right--;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-
-        System.out.println("Welcome to Palindrome Checker App");
-
-        String word = "madam";
-        String reversed = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
-
-        if (word.equals(reversed)) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
-        }
+        String input = "Madam In Eden Im Adam";
+        System.out.println(isPalindrome(input) ? "Palindrome" : "Not Palindrome");
     }
 }
